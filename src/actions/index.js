e7e7133b10f2db1af3e405b27c812d4ae3be4c28
Hans <hans.iv.71@gmail.com>
2023-01-13 18:51:17 -0500

    login1

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,9 @@
+import { Server } from "socket.io";
 export const GET_LOGIN = 'GET_LOGIN';
+export function getAllUsers() {
+    return function(dispatch) {
+      return fetch("https://jsonplaceholder.typicode.com/users")
+        .then(response => response.json())
+        .then(json => dispatch({ type: GET_LOGIN, payload: json }))
+    };
+  }
